d07fe32e8deb7af635146b45b3ee27434bbd123e
Muslimbek Nosirov <muslimbeknosirov1995@gmail.com>
2026-06-06 19:38:52 +0500

    fix(infra): PART1 remove dead Sprint4MigrationService (ow_orders not in DB)

--- apps/api/src/infrastructure/database/database.module.ts
+++ apps/api/src/infrastructure/database/database.module.ts
@@ -6,14 +6,13 @@
 import { Global, Module } from '@nestjs/common';
 import { Database } from './database';
 import { Sprint2MigrationService } from './sprint2-migration.service';
-import { Sprint4MigrationService } from './sprint4-migration.service';
 import { Sprint5MigrationService } from './sprint5-migration.service';
 import { Sprint6MigrationService } from './sprint6-migration.service';
 import { CrmMigrationService } from './crm-migration.service';
 
 @Global()
 @Module({
-  providers: [Database, Sprint2MigrationService, Sprint4MigrationService, Sprint5MigrationService, Sprint6MigrationService, CrmMigrationService],
+  providers: [Database, Sprint2MigrationService, Sprint5MigrationService, Sprint6MigrationService, CrmMigrationService],
   exports: [Database],
 })
 export class DatabaseModule {}
